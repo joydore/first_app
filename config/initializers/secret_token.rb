262e3b1d4fc781f1d1da8fac4010b2caddd27bae
Joy Dore <joy.dore@sage.com>
2014-05-23 16:09:16 +0100

    Inital versions of files, generated by railes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '5cfe41ea4e3b9f2b9221e0ced9cc8ab04c0666241c886ededc5abf9d8c45278b07bebb8440df249a069cc62b916037fb426468146d3a13b828f07bfbe148d1db'
